ad9481445785385c946673311d0bed7866af8dac
kishikk <k.kishi.yeah@gmail.com>
2021-03-25 06:06:23 +0900

    課題2完了

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,11 +2,18 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-
-}
+    $size = count($array);
+    for ($i = 0; $i < $size; $i++) {
+        for ($j = 1; $j < ($size -$i); $j++) {
+            if ($array[$j] < $array[$j - 1]) {
+                $num1 = $array[$j];
+                $array[$j] = $array[$j - 1];
+                $array[$j - 1] = $num1;
+            }
+        }
+    }
 // 修正はここまで
-
 echo "<pre>";
-print_r($array);
+print_r ($array);
 echo "</pre>";
+?>
